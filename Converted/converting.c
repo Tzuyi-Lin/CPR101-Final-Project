6917cf9d538862b6d2f777b721c9f8df16982493
Tzuyi-Lin <tlin59@myseneca.ca>
2024-07-27 15:06:26 -0400

    V1 converting

--- Converted/converting.c
+++ Converted/converting.c
@@ -4,6 +4,27 @@
 void converting() {
 /* Version 1 */
 // >> insert here
+    printf("*** Start of Converting Strings to int Demo ***\n");
+
+    char intString[BUFFER_SIZE];
+    int intNumber;
+
+    do
+    {
+        printf("Type an numeric string (q - to quit):\n");
+
+        fgets(intString, BUFFER_SIZE, stdin);
+        intString[strlen(intString) - 1] = '\0';
+
+        if (strcmp(intString, "q") != 0)
+        {
+            intNumber = atoi(intString);
+            printf("Converted number is %d\n", intNumber);
+        }
+
+    } while (strcmp(intString, "q") != 0);
+
+    printf("*** End of Converting String to int Demo ***\n\n");
 
 
 /* Version 2 */
---
+++ Converted/converting.h
@@ -0,0 +1,10 @@
+// CONVERTING MODULE HEADER
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void converting(void);
+#endif
