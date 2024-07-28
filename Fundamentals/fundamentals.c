61b807a4180c259813faf4845903895d36209490
Nina <jolin8722@yahoo.com.tw>
2024-07-28 11:51:33 -0400

    0728 edit fundamentals and manipulating

--- Fundamentals/fundamentals.c
+++ Fundamentals/fundamentals.c
@@ -1,16 +1,45 @@
+// FUNDAMENTALS MODULE SOURCE
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#define NUM_INPUT_SIZE 10
 #include "fundamentals.h"
 
-void fundamentals() {
-/* Version 1 */
->> insert here
+void fundamentals(void)
+{
+  // V1
+  printf("*** Start of Indexing Strings Demo ***\n");
+  char buffer1[BUFFER_SIZE];
+  char numInput[NUM_INPUT_SIZE];
+  size_t position;
 
+  // Exit the program with 'q' string
+  do
+  {
+    printf("Type not empty string (q - to quit):\n");
+    fgets(buffer1, BUFFER_SIZE, stdin);
 
-/* Version 2 */
->> insert here
+    buffer1[strlen(buffer1) - 1] = '\0';
 
+    if (strcmp(buffer1, "q") != 0)
+    {
+      printf("Type the character position within the string:\n");
+      fgets(numInput, NUM_INPUT_SIZE, stdin);
+      numInput[strlen(numInput) - 1] = '\0';
+      position = atoi(numInput);
 
-/* Version 3 */
->> insert here
+      // Validate if typed position match the maximum lenght of the the typed string
+      if (position >= strlen(buffer1))
+      {
+        // Asign the typed position to the length of buffer1 - 1, which is the length of the
+        // typed string by the user
+        position = strlen(buffer1) - 1;
+        printf("Too big... Position reduced to max. available\n");
+      }
+      printf("The character found at %d position is \'%c\'\n",
+             (int)position, buffer1[position]);
+    }
+  } while (strcmp(buffer1, "q") != 0);
 
-
+  // End execution of the program after user types 'q' string
+  printf("*** End of Indexing String Demo ***\n\n");
 }
--- Manipulating/manipulating.c
+++ Manipulating/manipulating.c
@@ -1,16 +1,39 @@
+// header file
 #include "manipulating.h"
+// standard input library
+#include <stdio.h>
+// string library
+#include <string.h>
 
-void manipulating() {
-/* Version 1 */
->> insert here
+void manipulating()
+{
+  /* Version 1 */
+  printf("*** Start of Concatenating String Demo ***\n");
+  // 2 strings of size 80
+  char string1[BUFFER_SIZE];
+  char string2[BUFFER_SIZE];
+  do
+  {
+    printf("Type the first string (q - to quit):\n");
+    fgets(string1, BUFFER_SIZE, stdin);
+    // make the last element of the entered string the NULL terminator to signify the end of the string
+    string1[strlen(string1) - 1] = '\0';
+    // if statement to check whether the user decides to quit with command q
+    if ((strcmp(string1, "q") != 0))
+    {
+      printf("Type the second string:\n");
+      fgets(string2, BUFFER_SIZE, stdin);
+      // make the last element of the entered string the NULL terminator to signify the end of the string
+      string2[strlen(string2) - 1] = '\0';
+      // append string2 to string1
+      strcat(string1, string2);
+      // result of the manipulation
+      printf("Concatenated string is \'%s\'\n", string1);
+    }
+    // iterate only if user did not enter q for the first string
+  } while ((strcmp(string1, "q") != 0));
 
+  /* Version 2 */
 
-/* Version 2 */
->> insert here
-
-
-/* Version 3 */
->> insert here
-
-
+  /* Version 3 */
 }
