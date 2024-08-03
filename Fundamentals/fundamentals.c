ffd9c9e22b40679cae4d41899dd0395b67737f6b
Nina <jolin8722@yahoo.com.tw>
2024-08-02 21:02:12 -0400

    V2_Fundamentals

--- Fundamentals/fundamentals.c
+++ Fundamentals/fundamentals.c
@@ -44,17 +44,17 @@ void fundamentals(void)
   printf("*** End of Indexing String Demo ***\n\n");
 
   /* Version 2 */
-  printf("*** Start of Measuring Strings Demo ***\n");
-  char buffer2[80];
-  printf("Type a string (q - to quit):\n");
-  gets(buffer2);
-  while (strcmp(buffer2, "q") != 0)
+  printf("*** Start of Measuring Strings Demo ***(n");
+  char buffer2[BUFFER_SIZE];
+  do
   {
-    printf("The length is %lu\n", strlen(buffer2));
     printf("Type a string (q - to quit):\n");
-    gets(buffer2);
-  }
-  printf("*** End of Measuring Strings Demo ***\n\n"); // Statement printed if q is entered.
+    fgets(buffer2, BUFFER_SIZE, stdin);
+    buffer2[strlen(buffer2) - 1] = '\0';
+    if (strcmp(buffer2, "q") != 0)
+      printf("The length of \'%s\' is %d characters\n", buffer2, (int)strlen(buffer2));
+  } while (strcmp(buffer2, "q") != 0);
+  printf("*** End of Measuring strings Demo ***(n)n");
 
   /* Version 3 */
 }
